48dd3453738aeebb5954ea7362a93a0da89b2522
DulceHQ <angelica.quiris@gmail.com>
2017-04-07 00:09:16 -0600

    Caso1

---
+++ Caso 1/mediacontaminante.R
@@ -0,0 +1,17 @@
+ setwd("~/GitHub/Programacion-Actuarial-III/specdata")
+
+ mediacontaminante <- function(directorio, contaminante, id = 1:332) {
+
+    yo <- numeric()
+    for (i in id) {
+    tu <- read.csv(paste(directorio, "/",formatC(i, width = 3, flag = "0"),".csv",sep = ""))
+    yo <- c(yo, tu[[contaminante]])
+ }
+  return(mean(yo, na.rm = TRUE))
+}
+
+mediacontaminante("specdata", "nitrate", 2:7)
+
+
+#dir.create("specdata")
+
